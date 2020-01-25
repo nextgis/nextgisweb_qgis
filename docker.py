17f4b9646a23b478e45b5893795790e1bfa4c6bb
Aleksandr Dezhin <me@dezhin.net>
2020-01-25 15:49:35 +0300

    Support for next ngwdocker version

--- docker.py
+++ docker.py
@@ -1,6 +1,3 @@
-import sys
-import click
-
 from ngwdocker import PackageBase
 
 class Package(PackageBase):
@@ -18,3 +15,22 @@ class Package(PackageBase):
             'COPY package/nextgisweb_qgis/qgis-to-env /opt/ngw/build/nextgisweb_qgis-qgis-to-env',
             'RUN /opt/ngw/build/nextgisweb_qgis-qgis-to-env /opt/ngw/env',
         )
+
+    def initialize(self):
+        super().initialize()
+
+        mod = self.context.packages['ngwdocker'].module
+        app_img = mod.AppImage
+
+        @app_img.on_apt.handler
+        def on_apt(event):
+            event.package('ngqgis', 'python-ngqgis', 'python-sip', 'python-qt4')
+
+        @app_img.on_package_files.handler
+        def on_package_files(event):
+            if event.package == self:
+                event.add(self.path / 'qgis-to-env')
+        
+        @app_img.on_virtualenv.handler
+        def on_virtualenv(event):
+            event.before_install('/opt/ngw/package/nextgisweb_qgis/qgis-to-env /opt/ngw/env')
