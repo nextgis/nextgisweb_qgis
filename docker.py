56a538ebd357b1ecf797741edd84d35b1341c55d
Aleksandr Dezhin <me@dezhin.net>
2020-08-05 05:36:39 +0300

    Move to qgis_headless master branch and qgis ltr version

--- docker.py
+++ docker.py
@@ -9,5 +9,5 @@ class Package(PackageBase):
 @AppImage.on_apt.handler
 def on_apt(event):
     event.command("apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 51F523511C7028C3")  # NOQA: E501
-    event.add_repository("deb https://qgis.org/ubuntu bionic main")
+    event.add_repository("deb https://qgis.org/ubuntu-ltr bionic main")
     event.package('build-essential', 'cmake', 'libqgis-dev')
