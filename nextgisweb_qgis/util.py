0ebca5e4c3efa53c28fc221819850db8fc04f65a
Eduard Popov <ed.popov.ru@yandex.ru>
2021-11-30 12:10:18 +1000

    Upgrade qgis_headless

--- nextgisweb_qgis/util.py
+++ nextgisweb_qgis/util.py
@@ -1,5 +1,3 @@
-from io import BytesIO
-
 from PIL import Image
 
 from nextgisweb.i18n import trstring_factory
@@ -9,4 +7,4 @@ _ = trstring_factory(COMP_ID)
 
 
 def qgis_image_to_pil(src):
-    return Image.open(BytesIO(src.to_bytes()))
+    return Image.frombytes('RGBA', src.size(), src.to_bytes().tobytes(), 'raw')
