33d98f25460ae3fe07ee2b79d8ff46307c2cf4de
Aleksandr Dezhin <me@dezhin.net>
2023-08-17 19:54:16 +0000

    Rename constraints in migrations
    CU-85ztpvc09

--- nextgisweb_qgis/migration/3ee6d7b3-sld-fileobj-type.fw.sql
+++ nextgisweb_qgis/migration/3ee6d7b3-sld-fileobj-type.fw.sql
@@ -4,12 +4,14 @@
     "message": "SLD fileobj type"
 } ***/
 ALTER TABLE qgis_raster_style RENAME qml_fileobj_id TO qgis_fileobj_id;
+ALTER TABLE qgis_raster_style RENAME CONSTRAINT qgis_raster_style_qml_fileobj_id_fkey TO qgis_raster_style_qgis_fileobj_id_fkey;
 ALTER TABLE qgis_raster_style ADD COLUMN qgis_format character varying(50);
 UPDATE qgis_raster_style
     SET qgis_format = CASE WHEN qgis_fileobj_id IS NOT NULL THEN 'qml_file' ELSE 'default' END;
 ALTER TABLE qgis_raster_style ALTER COLUMN qgis_format SET NOT NULL;
 
 ALTER TABLE qgis_vector_style RENAME qml_fileobj_id TO qgis_fileobj_id;
+ALTER TABLE qgis_vector_style RENAME CONSTRAINT qgis_vector_style_qml_fileobj_id_fkey TO qgis_vector_style_qgis_fileobj_id_fkey;
 ALTER TABLE qgis_vector_style ADD COLUMN qgis_format character varying(50);
 UPDATE qgis_vector_style
     SET qgis_format = CASE WHEN qgis_fileobj_id IS NOT NULL THEN 'qml_file' ELSE 'default' END;
--- nextgisweb_qgis/migration/3ee6d7b3-sld-fileobj-type.rw.sql
+++ nextgisweb_qgis/migration/3ee6d7b3-sld-fileobj-type.rw.sql
@@ -1,7 +1,9 @@
 /*** { "revision": "3ee6d7b3" } ***/
 
 ALTER TABLE qgis_raster_style DROP COLUMN qgis_format;
+ALTER TABLE qgis_raster_style RENAME CONSTRAINT qgis_raster_style_qgis_fileobj_id_fkey TO qgis_raster_style_qml_fileobj_id_fkey;
 ALTER TABLE qgis_raster_style RENAME qgis_fileobj_id TO qml_fileobj_id;
 
 ALTER TABLE qgis_vector_style DROP COLUMN qgis_format;
+ALTER TABLE qgis_vector_style RENAME CONSTRAINT qgis_vector_style_qgis_fileobj_id_fkey TO qgis_vector_style_qml_fileobj_id_fkey;
 ALTER TABLE qgis_vector_style RENAME qgis_fileobj_id TO qml_fileobj_id;
