88ad958b284757cc7bf06c38032b25e073c13954
Eduard Popov <ed.popov.ru@yandex.ru>
2023-09-05 18:00:09 +1000

    Deprecated attributes setters

--- nextgisweb_qgis/model.py
+++ nextgisweb_qgis/model.py
@@ -148,12 +148,26 @@ class QgisStyleMixin:
             "instead of qgis_style.qml_fileobj_id", DeprecationWarning, 2)
         return self.qgis_fileobj_id
 
+    @qml_fileobj_id.setter
+    def qml_fileobj_id(self, value):
+        warn(
+            "Since nextgisweb_gis 2.9.0.dev1 use qgis_style.qgis_fileobj_id "
+            "instead of qgis_style.qml_fileobj_id", DeprecationWarning, 2)
+        self.qgis_fileobj_id = value
+
     @property
     def qml_fileobj(self):
         warn(
             "Since nextgisweb_gis 2.9.0.dev1 use qgis_style.qgis_fileobj "
             "instead of qgis_style.qml_fileobj", DeprecationWarning, 2)
         return self.qgis_fileobj
+
+    @qml_fileobj.setter
+    def qml_fileobj(self, value):
+        warn(
+            "Since nextgisweb_gis 2.9.0.dev1 use qgis_style.qgis_fileobj "
+            "instead of qgis_style.qml_fileobj", DeprecationWarning, 2)
+        self.qgis_fileobj = value
 
 
 @implementer(IRenderableStyle)
@@ -454,7 +468,7 @@ class _file_upload_attr(SP):
             _reraise_qgis_exception(exc, ValidationError)
 
         fileobj = env.file_storage.fileobj(component=COMP_ID)
-        srlzr.obj.qgis_fileobj = fileobj
+        srlzr.obj.qml_fileobj = fileobj
         dstfile = env.file_storage.filename(fileobj, makedirs=True)
 
         copyfile(srcfile, dstfile)
