d3bf9ce5d965c0329ba29cc34454da08758792a7
Aleksandr Dezhin <me@dezhin.net>
2024-03-25 12:19:16 +0000

    Switch to generated API types

--- nextgisweb_qgis/nodepkg/default-style-widget/DefaultStyleWidget.tsx
+++ nextgisweb_qgis/nodepkg/default-style-widget/DefaultStyleWidget.tsx
@@ -5,10 +5,7 @@ import { errorModal } from "@nextgisweb/gui/error";
 import type { ApiError } from "@nextgisweb/gui/error/type";
 import { route, routeURL } from "@nextgisweb/pyramid/api";
 import { gettext } from "@nextgisweb/pyramid/i18n";
-import type {
-    ResourceItem,
-    ResourceItemCreationResponse,
-} from "@nextgisweb/resource/type/Resource";
+import type { CompositeCreate } from "@nextgisweb/resource/type/api";
 
 // prettier-ignore
 const [msgButton, msgText] = [
@@ -16,16 +13,10 @@ const [msgButton, msgText] = [
     gettext("Layer created. You need a style to add it to a web map. Use the button bellow to create a default QGIS style or create a style resource using sidebar."),
 ]
 
-export function DefaultStyleWidget({
-    payload,
-}: {
-    payload: Partial<ResourceItem>;
-}) {
+export function DefaultStyleWidget({ payload }: { payload: CompositeCreate }) {
     const create = async () => {
         try {
-            const { id } = await route(
-                "resource.collection"
-            ).post<ResourceItemCreationResponse>({
+            const { id } = await route("resource.collection").post({
                 json: payload,
             });
             window.open(routeURL("resource.show", { id }), "_self");
